3de25d0e9bfcfb4438db975a86943236e98e560a
camxus <camillus.konkwo@gmail.com>
2025-09-04 18:04:28 +0200

    fix filter

--- app/[username]/[projectName]/page.tsx
+++ app/[username]/[projectName]/page.tsx
@@ -214,10 +214,12 @@ export default function PublicProjectPage() {
 
     const filteredRatings = Object.values(ratings).filter(
       ({ user_id, value }) => {
-        const userMatch = !!userIds.length || userIds.includes(user_id);
+        const userMatch = !!userIds.length ? userIds.includes(user_id) : true;
         const ratingMatch =
-          !!ratingValues.length || ratingValues.includes(value);
-        return userMatch || ratingMatch;
+          !!ratingValues.length ? ratingValues.includes(value) : true;
+
+          console.log(userMatch, ratingMatch)
+        return userMatch && ratingMatch;
       }
     );
 
@@ -227,6 +229,12 @@ export default function PublicProjectPage() {
 
     setFilteredImages(filteredImages);
   };
+
+  const handleDuplicateImage = () => {
+    if (!isCurrentUser) return
+
+    // updateProject({projectId: project?.project_id, })
+  }
 
   useEffect(() => {
     loadProject();
@@ -301,6 +309,7 @@ export default function PublicProjectPage() {
               }}
               onRatingChange={handleRatingChange}
               onImageHoverChange={(hover) => setIsHovered(hover)}
+              onDuplicateImage={handleDuplicateImage}
             />
             <ImageCarousel
               images={filteredImages}
