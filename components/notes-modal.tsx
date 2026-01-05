42cdd3341a1d5869cae68538a526d93ad29e4506
camxus <camillus.konkwo@gmail.com>
2026-01-05 19:02:07 +0100

    increase rounded

--- components/notes-modal.tsx
+++ components/notes-modal.tsx
@@ -204,7 +204,7 @@ export function NotesModal({ projectId, imageName }: NotesViewProps) {
                 return (
                   <div
                     key={`${firstNote.note_id}-${groupIndex}`}
-                    className="flex flex-col gap-2 bg-background rounded-sm p-2 shadow-xl border-foreground/60"
+                    className="flex flex-col gap-2 bg-background rounded-lg p-2 shadow-xl border-foreground/60"
                   >
                     {/* Author header */}
                     <div className="flex items-start gap-2">
